005f3e79de37794968d1412e5d75fc74a81bda52
athelvaci <athelvaci@gmail.com>
2018-04-02 00:24:56 +0300

    first commit

---
+++ src/main/java/com/example/polls/security/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.example.polls.security;
+
+import java.lang.annotation.Target;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
